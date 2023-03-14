c919ef07b6b39df3bfaaa61a903894974b769d92
shehab <sheshox12@gmail.com>
2023-03-14 03:34:48 +0200

    create main activity implementation

--- app/src/main/java/com/projects/programmercalculator/MainActivity.kt
+++ app/src/main/java/com/projects/programmercalculator/MainActivity.kt
@@ -1,143 +1,176 @@
 package com.projects.programmercalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 
-//limit num for crashes
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnConvert: Button
-    private lateinit var btnClear: Button
-    private lateinit var etBin: EditText
-    private lateinit var etOct: EditText
-    private lateinit var etDec: EditText
-    private lateinit var etHex: EditText
-    private lateinit var binOutput: String
-    private lateinit var octOutput: String
-    private lateinit var decOutput: String
-    private lateinit var hexOutput: String
-//    private lateinit var btnClearLog: Button
-
+    private lateinit var editTextBinary: TextInputLayout
+    private lateinit var editTextOctal: TextInputLayout
+    private lateinit var editTextDecimal: TextInputLayout
+    private lateinit var editTextHexadecimal: TextInputLayout
+    private lateinit var outputBinary: String
+    private lateinit var outputOctal: String
+    private lateinit var outputDecimal: String
+    private lateinit var outputHexadecimal: String
+    private lateinit var buttonClear: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // supportActionBar!!.setBackgroundDrawable(ColorDrawable(getColor(R.))))
         initViews()
-
-        btnConvert.setOnClickListener {
-            convertNumbers()
+        onTextChanged()
+        buttonClear.setOnClickListener {
+            clearEditTextInputs()
         }
-
-        btnClear.setOnClickListener {
-            clearViewsInput()
-        }
-
     }
 
+    private fun onTextChanged() {
+        var base: Int
 
-    private fun convertNumbers() {
-        //while ((this.currentFocus as EditText).text.toString().toLong() <= Long.MAX_VALUE) {
-            when(this.currentFocus?.id) {
-                etBin.id -> {
-                    convertBinaryToAll()
-                }
-                etDec.id -> {
-                    convertDecimalToAll()
-                }
-                etOct.id -> {
-                    convertOctalToAll()
-                }
-                etHex.id -> {
-                    convertHexToAll()
-                }
-                else -> Toast.makeText(this,"Enter number first", Toast.LENGTH_SHORT).show()
+        editTextBinary.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
-     //   }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                base = BINARY_BASE
+                convertNumber(base, editTextBinary)
+            }
+        })
+
+        editTextOctal.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                base = OCTAL_BASE
+                convertNumber(base, editTextOctal)
+
+            }
+        })
+
+        editTextDecimal.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                base = DECIMAL_BASE
+                convertNumber(base, editTextDecimal)
+            }
+        })
+
+        editTextHexadecimal.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                base = HEXADECIMAL_BASE
+                convertNumber(base, editTextHexadecimal)
+            }
+        })
     }
 
-    private fun convertHexToAll() {
-        if (areAllInputsNotEmpty()) {
-            val hexInput = etHex.text.toString()
-            decOutput = hexInput.toLong(HEXADECIMAL_BASE).toString()
-            binOutput = decOutput.toLong().toString(BINARY_BASE)
-            octOutput = decOutput.toLong().toString(OCTAL_BASE)
-            etDec.setText(decOutput)
-            etBin.setText(binOutput)
-            etOct.setText(octOutput)
-
+    private fun convertNumber(base: Int, editText: TextInputLayout) {
+        if (this@MainActivity.currentFocus?.id == editText.editText?.id
+            && editText.editText?.text?.isNotEmpty() == true
+        ) {
+            updateEditTextInputValues(base, editText)
+        }
+        if (editText.editText?.text?.isEmpty() == true) {
+            clearEditTextInputs()
         }
     }
 
-    private fun convertOctalToAll() {
-        if (areAllInputsNotEmpty()) {
-            val octInput = etOct.text.toString()
-            decOutput = octInput.toLong(OCTAL_BASE).toString()
-            binOutput = decOutput.toLong().toString(BINARY_BASE)
-            hexOutput = decOutput.toLong().toString(HEXADECIMAL_BASE)
-            etDec.setText(decOutput)
-            etBin.setText(binOutput)
-            etHex.setText(hexOutput)
+    private fun updateEditTextInputValues(base: Int, editText: TextInputLayout) {
+        val input = editText.editText?.text.toString()
+        outputDecimal = input.toLong(base).toString()
+        outputBinary = outputDecimal.toLong().toString(BINARY_BASE)
+        outputOctal = outputDecimal.toLong().toString(OCTAL_BASE)
+        outputHexadecimal = outputDecimal.toLong().toString(HEXADECIMAL_BASE)
+        setEditTextViewsTexts(editText)
+    }
 
+    private fun setEditTextViewsTexts(editText: TextInputLayout) {
+        when (editText.id) {
+            editTextBinary.id -> {
+                editTextDecimal.editText?.setText(outputDecimal)
+                editTextOctal.editText?.setText(outputOctal)
+                editTextHexadecimal.editText?.setText(outputHexadecimal)
+            }
+            editTextOctal.id -> {
+                editTextDecimal.editText?.setText(outputDecimal)
+                editTextBinary.editText?.setText(outputBinary)
+                editTextHexadecimal.editText?.setText(outputHexadecimal)
+            }
+            editTextHexadecimal.id -> {
+                editTextDecimal.editText?.setText(outputDecimal)
+                editTextOctal.editText?.setText(outputOctal)
+                editTextBinary.editText?.setText(outputBinary)
+            }
+            editTextDecimal.id -> {
+                editTextBinary.editText?.setText(outputBinary)
+                editTextOctal.editText?.setText(outputOctal)
+                editTextHexadecimal.editText?.setText(outputHexadecimal)
+            }
         }
     }
 
-    private fun convertDecimalToAll() {
-        if (areAllInputsNotEmpty()) {
-            val decInput = etDec.text.toString()
-            binOutput = decInput.toLong().toString(BINARY_BASE)
-            octOutput = decInput.toLong().toString(OCTAL_BASE)
-            hexOutput = decInput.toLong().toString(HEXADECIMAL_BASE)
-            etBin.setText(binOutput)
-            etOct.setText(octOutput)
-            etHex.setText(hexOutput)
-        }
-    }
+//    private fun areAllEditTextNotEmpty(): Boolean {
+//        return (editTextBinary.text.isNotEmpty()
+//                || editTextOctal.text.isNotEmpty()
+//                || editTextDecimal.text.isNotEmpty()
+//                || editTextHexadecimal.editText?.text?.isNotEmpty() == true)
+//    }
 
 
-    private fun convertBinaryToAll() {
-        if (areAllInputsNotEmpty()) {
-            val binaryInput = etBin.text.toString()
-            decOutput = binaryInput.toLong(BINARY_BASE).toString()
-            octOutput = binaryInput.toLong(BINARY_BASE).toString(OCTAL_BASE)
-            hexOutput = binaryInput.toLong(BINARY_BASE).toString(HEXADECIMAL_BASE)
-            etDec.setText(decOutput)
-            etOct.setText(octOutput)
-            etHex.setText(hexOutput)
-        }
-    }
-
-    private fun areAllInputsNotEmpty(): Boolean {
-        return (etBin.text.isNotEmpty()
-                || etOct.text.isNotEmpty()
-                || etDec.text.isNotEmpty()
-                || etHex.text.isNotEmpty())
-    }
-
-
-    private fun clearViewsInput() {
-        etBin.setText("")
-        etOct.setText("")
-        etDec.setText("")
-        etHex.setText("")
+    private fun clearEditTextInputs() {
+        editTextBinary.editText?.text?.clear()
+        editTextOctal.editText?.text?.clear()
+        editTextDecimal.editText?.text?.clear()
+        editTextHexadecimal.editText?.text?.clear()
     }
 
     private fun initViews() {
-        btnConvert = findViewById(R.id.btn_convert)
-        btnClear = findViewById(R.id.btn_clear)
-        etBin = findViewById(R.id.et_bin)
-        etOct = findViewById(R.id.et_oct)
-        etDec = findViewById(R.id.et_dec)
-        etHex = findViewById(R.id.et_hex)
-    }
-
-    private companion object {
-        const val BINARY_BASE = 2
-        const val OCTAL_BASE = 8
-        const val DECIMAL_BASE = 10
-        const val HEXADECIMAL_BASE = 16
+        editTextBinary = findViewById(R.id.text_input_layout_binary)
+        editTextOctal = findViewById(R.id.text_input_layout_octal)
+        editTextDecimal = findViewById(R.id.text_input_layout_decimal)
+        editTextHexadecimal = findViewById(R.id.text_input_layout_hexadecimal)
+        buttonClear = findViewById(R.id.btn_clear)
     }
 
 }
